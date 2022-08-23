8457fa9dab9d1b17b78f63366fb66131f38cdb52
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-23 16:12:33 +0900

    ニュース記事の一覧表示

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import ListItem from './components/ListItem';
+import articles from './dummies/articles.json';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,29 +14,19 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={index}
+      />
+    );
+  });
   return (
     <View style={styles.container}>
-      <ListItem
-        imageUrl="https://picsum.photos/id/237/200/300"
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat."
-        author="SampleNews"
-      />
-      <ListItem
-        imageUrl="https://picsum.photos/id/237/200/300"
-        title="Hello World"
-        author="SampleNews"
-      />
-      <ListItem
-        imageUrl="https://picsum.photos/id/237/200/300"
-        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat."
-        author="SampleNews"
-      />
+      {items}
       <StatusBar style="auto" />
     </View>
   );
