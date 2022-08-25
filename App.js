49be5680e7921ca6b2ac0b76ebfb045dbed2cd8e
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-25 15:34:40 +0900

    Axiosを使ってAPIを実行

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/articles.json';
 import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,12 +19,17 @@ const styles = StyleSheet.create({
 export default function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    alert(Constants.manifest.extra.newsApiKey);
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 2000);
-    return () => clearImmediate(timer);
+    fetchArticles();
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
