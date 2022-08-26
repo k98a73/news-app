cec63b98825671126a5c7201f62ebdf5df4cb56d
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-26 16:32:49 +0900

    StackNavigatorの利用

---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+import ListItem from '../components/ListItem';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default function HomeScreen() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
