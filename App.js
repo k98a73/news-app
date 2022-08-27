7a47bc50bc2cf34dcc095a85a616e64e6d70428d
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-27 17:26:20 +0900

    Storeの実装

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import AppNavigator from './navigation/AppNavigator';
+import store from './store';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
---
+++ store/index.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from 'redux';
+
+import userReducer from './reducers/user';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
