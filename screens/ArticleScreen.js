0dbafb66c12b1e3e5543541dcb445b06e893b868
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-27 16:40:21 +0900

    ニュース記事を表示するためにWebviewを表示

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,7 +13,7 @@ const styles = StyleSheet.create({
 export default function ArticleScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is Article Screen</Text>
+      <WebView source={{ uri: 'https://expo.dev' }} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
