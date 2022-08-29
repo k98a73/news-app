eb3f186bbd905b30f06fb338ce1917cb9255b1ca
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-29 09:32:09 +0900

    デバッガーのインストール

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import userReducer from './reducers/user';
 
@@ -6,6 +7,6 @@ const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
