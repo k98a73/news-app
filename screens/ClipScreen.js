b29845001ebc75913e199501e5aacf7a2be70dc8
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-30 17:05:32 +0900

    画面の下にタブを表示

---
+++ screens/ClipScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>CLIP Screen</Text>
+    </SafeAreaView>
+  );
+};
