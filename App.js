e8cff5f0bfabb0d48023d256b76a9cb2edf0d3e0
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-01 11:31:34 +0900

    Reduxを永続化して端末に保存

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import AppNavigator from './navigation/AppNavigator';
-import store from './store';
+import store, { persistor } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppNavigator />
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.js
+++ store/index.js
@@ -1,5 +1,7 @@
+import { AsyncStorage } from 'react-native';
 import { combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
 
 import userReducer from './reducers/user';
 
@@ -7,6 +9,15 @@ const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: ' root',
+  storage: AsyncStorage,
+  whiteList: ['user'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
