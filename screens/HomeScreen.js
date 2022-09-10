3ec50d1f7c4c4c591cb348ad3960250d2db5a3b5
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-10 16:04:29 +0900

    無限スクロール(ページング)の実装

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import {
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 import Constants from 'expo-constants';
 import axios from 'axios';
 
@@ -19,24 +24,51 @@ const styles = StyleSheet.create({
 export default HomeScreen = ({ navigation }) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const pageRef = useRef(1);
+  const fetchedAllRef = useRef(false);
 
   useEffect(() => {
-    fetchArticles();
+    setLoading(true);
+    fetchArticles(1);
+    setLoading(false);
   }, []);
 
-  const fetchArticles = async () => {
-    setLoading(true);
+  const fetchArticles = async (page) => {
     try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
+      const response = await axios.get(`${URL}&page=${page}`);
+      if (response.data.articles.length > 0) {
+        setArticles((prevArticles) => [
+          ...prevArticles,
+          ...response.data.articles,
+        ]);
+      } else {
+        fetchedAllRef.current = true;
+      }
     } catch (error) {
       console.error(error);
     }
-    setLoading(false);
+  };
+
+  const onEndReached = () => {
+    if (!fetchedAllRef.current) {
+      pageRef.current = pageRef.current + 1;
+      fetchArticles(pageRef.current);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    setArticles([]);
+    pageRef.current = 1;
+    fetchedAllRef.current = false;
+    await fetchArticles(1);
+    setRefreshing(false);
   };
 
   return (
     <SafeAreaView style={styles.container}>
+      {loading && <Loading />}
       <FlatList
         data={articles}
         renderItem={({ item }) => (
@@ -48,8 +80,11 @@ export default HomeScreen = ({ navigation }) => {
           />
         )}
         keyExtractor={(item, index) => index.toString()}
+        onEndReached={onEndReached}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
-      {loading && <Loading />}
       <StatusBar style="auto" />
     </SafeAreaView>
   );
